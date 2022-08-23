8f8a4de6f7a6146b624e5e05d01db095d13c1c1b
Alexandre Beiruth <101944430+Alebeiruth@users.noreply.github.com>
2022-08-23 16:39:02 +0000

    Initial commit

---
+++ .ccls-cache/@home@runner@FrizzyWhirlwindInterpreter/main.c
@@ -0,0 +1,251 @@
+#include <stdio.h>
+
+int main() {
+  // variaveis
+  unsigned int est;
+  unsigned int city;
+  int num;
+
+  // usuario e programa em execução
+  printf("Digite os primeiro número da sua placa, o segundo e em seguida os dois ultimos\n");
+  scanf("%d %d %d", &est, &city, &num);
+
+  // estado 0 e suas 10 primeiras cidades
+
+  if (est == 0 && city == 0) {
+    printf("Seu estado é o Acre e sua cidade é Acrelândia, os números da sua placa é %d%d%d ", est, city, num);
+  } else if (est == 0 && city == 1) {
+    printf("Seu estado é o Acre e sua cidade é Assis Brasil, os números da sua placa é %d%d%d ", est, city, num);
+  } else if (est == 0 && city == 2) {
+    printf("Seu estado é o Acree sua cidade é Brasiléa, os números da sua placa é %d%d%d ", est, city, num);
+  } else if (est == 0 && city == 3) {
+    printf("Seu estado é o Acre e sua cidade é Bujari, os números da sua placa é %d%d%d ", est, city, num);
+  } else if (est == 0 && city == 4) {
+    printf("Seu estado é o Acre e sua cidade é Capixaba, os números da sua placa é %d%d%d ", est, city, num);
+  } else if (est == 0 && city == 5) {
+    printf("Seu estado é o Acre e sua cidade é Cruzeiro do Su, os números da sua placa é %d%d%d ", est, city, num);
+  } else if (est == 0 && city == 6) {
+    printf("Seu estado é o Acre e sua cidade é Epitaciolaândia, os números da sua placa é %d%d%d ", est, city, num);
+  } else if (est == 0 && city == 7) {
+    printf("Seu estado é o Acre e sua cidade é Epitaciolaândia, os números da sua placa é %d%d%d ", est, city, num);
+  } else if (est == 0 && city == 8) {
+    printf("Seu estado é o Acre e sua cidade é Feijó, os números da sua placa é %d%d%d ", est, city, num);
+  } else if (est == 0 && city == 9) {
+    printf("Seu estado é o Acre e sua cidade é Rio Branco, os números da sua placa é %d%d%d", est, city, num);
+  }
+
+  // estado 1 e suas 10 primeiras cidades
+
+  if (est == 1 && city == 0) {
+    printf("Seu estado é o Alagoas e sua cidade é Alagoas, os números da sua placa é %d%d%d ", est, city, num);
+  } else if (est == 1 && city == 1) {
+    printf("Seu estado é o Alagoas e sua cidade é Anadia, os números da sua placa é %d%d%d ", est, city, num);
+  } else if (est == 1 && city == 2) {
+    printf("Seu estado é o Alagoas e sua cidade é Arapiraca, os números da sua placa é %d%d%d ", est, city, num);
+  } else if (est == 1 && city == 3) {
+    printf("Seu estado é o Alagoas e sua cidade é Atalaia, os números da sua placa é %d%d%d ", est, city, num);
+  } else if (est == 1 && city == 4) {
+    printf("Seu estado é o Alagoas e sua cidade é Bandeiras, os números da sua placa é %d%d%d ", est, city, num);
+  } else if (est == 1 && city == 5) {
+    printf("Seu estado é o Alagoas e sua cidade é Barra de Santo Antônio, os números da sua placa é %d%d%d ", est, city, num);
+  } else if (est == 1 && city == 6) {
+    printf("Seu estado é o Alagoas e sua cidade é Barra de São Miguel, os números da sua placa é %d%d%d ", est, city, num);
+  } else if (est == 1 && city == 7) {
+    printf("Seu estado é o Alagoas e sua cidade é Batalha, os números da sua placa é %d%d%d ", est, city, num);
+  } else if (est == 1 && city == 8) {
+    printf("Seu estado é o Alagoas e sua cidade é Belém, os números da sua placa é %d%d%d ", est, city, num);
+  } else if (est == 1 && city == 9) {
+    printf("Seu estado é o Alagoas e sua cidade é Belo Monte, os números da sua placa é %d%d%d ", est, city, num);
+  }
+
+  // estado 2 e suas 10 primeiras cidades
+
+  if (est == 2 && city == 0) {
+    printf("Seu estado é o Amapá e sua cidade é Amapá, os números da sua placa é %d%d%d ", est, city, num);
+  } else if (est == 2 && city == 1) {
+    printf("Seu estado é o Amapá e sua cidade é Calçoene, os números da sua placa é %d%d%d ", est, city, num);
+  } else if (est == 2 && city == 2) {
+    printf("Seu estado é o Amapá e sua cidade é Cutias, os números da sua placa é %d%d%d ", est, city, num);
+  } else if (est == 2 && city == 3) {
+    printf("Seu estado é o Amapá e sua cidade é Ferreira Gomes, os números da sua placa é %d%d%d ", est, city, num);
+  } else if (est == 2 && city == 4) {
+    printf("Seu estado é o Amapá e sua cidade é Itaubal, os números da sua placa é %d%d%d ", est, city, num);
+  } else if (est == 2 && city == 5) {
+    printf("Seu estado é o Amapá e sua cidade é Laranja do Jaré, os números da sua placa é %d%d%d ", est, city, num);
+  } else if (est == 2 && city == 6) {
+    printf("Seu estado é o Amapá e sua cidade é Macapá, os números da sua placa é %d%d%d ", est, city, num);
+  } else if (est == 2 && city == 7) {
+    printf("Seu estado é o Amapá e sua cidade é Mazagão, os números da sua placa é %d%d%d ", est, city, num);
+  } else if (est == 2 && city == 8) {
+    printf("Seu estado é o Amapá e sua cidade é Oiaporque, os números da sua placa é %d%d%d ", est, city, num);
+  } else if (est == 2 && city == 9) {
+    printf("Seu estado é o Amapá e sua cidade é Pedra Branca do Amapari, os números da sua placa é %d%d%d ", est, city, num);
+  }
+  // estado 3 e suas 10 primeiras cidades
+
+  if (est == 3 && city == 0) {
+    printf("Seu estado é o Amazonas e sua cidade é Alvarães, os números da sua placa é %d%d%d ", est, city, num);
+  } else if (est == 3 && city == 1) {
+    printf("Seu estado é o Amazonas e sua cidade é Amaturá, os números da sua placa é %d%d%d ", est, city, num);
+  } else if (est == 3 && city == 2) {
+    printf("Seu estado é o Amazonas e sua cidade é Anamã, os números da sua placa é %d%d%d ", est, city, num);
+  } else if (est == 3 && city == 3) {
+    printf("Seu estado é o Amazonas e sua cidade é Anori, os números da sua placa é %d%d%d ", est, city, num);
+  } else if (est == 3 && city == 4) {
+    printf("Seu estado é o Amazonas e sua cidade é Apui, os números da sua placa é %d%d%d ", est, city, num);
+  } else if (est == 3 && city == 5) {
+    printf("Seu estado é o Amazonas e sua cidade é Atalaia do Norte, os números da sua placa é %d%d%d ", est, city, num);
+  } else if (est == 3 && city == 6) {
+    printf("Seu estado é o Amazonas e sua cidade é Autazes, os números da sua placa é %d%d%d ", est, city, num);
+  } else if (est == 3 && city == 7) {
+    printf("Seu estado é o Amazonas e sua cidade é Barcelos, os números da sua placa é %d%d%d ", est, city, num);
+  } else if (est == 3 && city == 8) {
+    printf("Seu estado é o Amazonas e sua cidade é Barrerinha, os números da sua placa é %d%d%d ", est, city, num);
+  } else if (est == 3 && city == 9) {
+    printf("Seu estado é o Amazonas e sua cidade é Pedra Benjamin Constant, os números da sua placa é %d%d%d ", est, city, num);
+  }
+
+  // estado 4 e suas 10 primeiras cidades
+
+  if (est == 4 && city == 0) {
+    printf("Seu estado é a Bahia e sua cidade é Abaíra, os números da sua placa é %d%d%d ", est, city, num);
+  } else if (est == 4 && city == 1) {
+    printf("Seu estado é a Bahia e sua cidade é Abaré, os números da sua placa é %d%d%d ", est, city, num);
+  } else if (est == 4 && city == 2) {
+    printf("Seu estado é a Bahia e sua cidade é Acajutiba, os números da sua placa é %d%d%d ", est, city, num);
+  } else if (est == 4 && city == 3) {
+    printf("Seu estado é a Bahia e sua cidade é Adustina, os números da sua placa é %d%d%d ", est, city, num);
+  } else if (est == 4 && city == 4) {
+    printf("Seu estado é a Bahia e sua cidade é Água Fria, os números da sua placa é %d%d%d ", est, city, num);
+  } else if (est == 4 && city == 5) {
+    printf("Seu estado é a Bahia e sua cidade é Aiquara, os números da sua placa é %d%d%d ", est, city, num);
+  } else if (est == 4 && city == 6) {
+    printf("Seu estado é a Bahia e sua cidade é Alcobaça, os números da sua placa é %d%d%d ", est, city, num);
+  } else if (est == 4 && city == 7) {
+    printf("Seu estado é a Bahia e sua cidade é Almadina, os números da sua placa é %d%d%d ", est, city, num);
+  } else if (est == 4 && city == 8) {
+    printf("Seu estado é a Bahia e sua cidade é Amargosa, os números da sua placa é %d%d%d ", est, city, num);
+  } else if (est == 4 && city == 9) {
+    printf("Seu estado é a Bahia e sua cidade é Amélia Rodrigues, os números da sua placa é %d%d%d ", est, city, num);
+  }
+
+  // estado 5 e suas 10 primeiras cidades
+
+  if (est == 5 && city == 0) {
+    printf("Seu estado é o Ceará e sua cidade é Abaiara, os números da sua placa é %d%d%d ", est, city, num);
+  } else if (est == 5 && city == 1) {
+    printf("Seu estado é o Ceará e sua cidade é Acarapé, os números da sua placa é %d%d%d ", est, city, num);
+  } else if (est == 5 && city == 2) {
+    printf("Seu estado é o Ceará e sua cidade é Acaraú, os números da sua placa é %d%d%d ", est, city, num);
+  } else if (est == 5 && city == 3) {
+    printf("Seu estado é o Ceará e sua cidade é Acopiara, os números da sua placa é %d%d%d ", est, city, num);
+  } else if (est == 5 && city == 4) {
+    printf("Seu estado é o Ceará e sua cidade é Aiuaba, os números da sua placa é %d%d%d ", est, city, num);
+  } else if (est == 5 && city == 5) {
+    printf("Seu estado é o Ceará e sua cidade é Alcântaras, os números da sua placa é %d%d%d ", est, city, num);
+  } else if (est == 5 && city == 6) {
+    printf("Seu estado é o Ceará e sua cidade é Altaneira, os números da sua placa é %d%d%d ", est, city, num);
+  } else if (est == 5 && city == 7) {
+    printf("Seu estado é o Ceará e sua cidade é Alto Santo, os números da sua placa é %d%d%d ", est, city, num);
+  } else if (est == 5 && city == 8) {
+    printf("Seu estado é o Ceará e sua cidade é Amontada, os números da sua placa é %d%d%d ", est, city, num);
+  } else if (est == 5 && city == 9) {
+    printf("Seu estado é o Ceará e sua cidade é Antonina do Norte, os números da sua placa é %d%d%d ", est, city, num);
+  }
+
+  // estado 6 e suas 10 primeiras cidades
+
+  if (est == 6 && city == 0) {
+    printf("Seu estado é Espirito Santo e sua cidade é Afonso Cláudio, os números da sua placa é %d%d%d ", est, city, num);
+  } else if (est == 6 && city == 1) {
+    printf("Seu estado é Espirito Santo e sua cidade é Água Doce do Norte, os números da sua placa é %d%d%d ", est, city, num);
+  } else if (est == 6 && city == 2) {
+    printf("Seu estado é Espirito Santo e sua cidade é Água Branca, os números da sua placa é %d%d%d ", est, city, num);
+  } else if (est == 6 && city == 3) {
+    printf("Seu estado é Espirito Santo e sua cidade é Alegre, os números da sua placa é %d%d%d ", est, city, num);
+  } else if (est == 6 && city == 4) {
+    printf("Seu estado é Espirito Santo e sua cidade é Alfredo Chaves, os números da sua placa é %d%d%d ", est, city, num);
+  } else if (est == 6 && city == 5) {
+    printf("Seu estado é Espirito Santo e sua cidade é Alto Rio Novo, os números da sua placa é %d%d%d ", est, city, num);
+  } else if (est == 6 && city == 6) {
+    printf("Seu estado é Espirito Santo e sua cidade é Anchieta, os números da sua placa é %d%d%d ", est, city, num);
+  } else if (est == 6 && city == 7) {
+    printf("Seu estado é Espirito Santo e sua cidade é Apiac, os números da sua placa é %d%d%d ", est, city, num);
+  } else if (est == 6 && city == 8) {
+    printf("Seu estado é Espirito Santo e sua cidade é Castelo, os números da sua placa é %d%d%d ", est, city, num);
+  } else if (est == 6 && city == 9) {
+    printf("Seu estado é Espirito Santo e sua cidade é Vitória, os números da sua placa é %d%d%d ", est, city, num);
+  }
+
+  // estado 7 e suas 10 primeiras cidades
+
+  if (est == 7 && city == 0) {
+    printf("Seu estado é o Goiás e sua cidade é Abadia de Goiás, os números da sua placa é %d%d%d ", est, city, num);
+  } else if (est == 7 && city == 1) {
+    printf("Seu estado é o Goiás e sua cidade é Abadiânia, os números da sua placa é %d%d%d ", est, city, num);
+  } else if (est == 7 && city == 2) {
+    printf("Seu estado é o Goiás e sua cidade é Ácreuna, os números da sua placa é %d%d%d ", est, city, num);
+  } else if (est == 7 && city == 3) {
+    printf("Seu estado é o Goiás e sua cidade é Adelândia, os números da sua placa é %d%d%d ", est, city, num);
+  } else if (est == 7 && city == 4) {
+    printf("Seu estado é o Goiás e sua cidade é Águas Fria de Goiás, os números da sua placa é %d%d%d ", est, city, num);
+  } else if (est == 7 && city == 5) {
+    printf("Seu estado é o Goiás e sua cidade é Águas Lindas de Goiás, os números da sua placa é %d%d%d ", est, city, num);
+  } else if (est == 7 && city == 6) {
+    printf("Seu estado é o Goiás e sua cidade é Alexânia, os números da sua placa é %d%d%d ", est, city, num);
+  } else if (est == 7 && city == 7) {
+    printf("Seu estado é o Goiás e sua cidade é Aloândia, os números da sua placa é %d%d%d ", est, city, num);
+  } else if (est == 7 && city == 8) {
+    printf("Seu estado é o Goiás e sua cidade é Alto Horizonte, os números da sua placa é %d%d%d ", est, city, num);
+  } else if (est == 7 && city == 9) {
+    printf("Seu estado é o Goiás e sua cidade é Alto Paraíso de Goiás, os números da sua placa é %d%d%d ", est, city, num);
+  }
+
+  // estado 8 e suas 10 primeiras cidades
+
+  if (est == 8 && city == 0) {
+    printf("Seu estado é o Maranhão e sua cidade é Abadia de Açailândia, os números da sua placa é %d%d%d ", est, city, num);
+  } else if (est == 8 && city == 1) {
+    printf("Seu estado é o Maranhão e sua cidade é Afonso Cunha, os números da sua placa é %d%d%d ", est, city, num);
+  } else if (est == 8 && city == 2) {
+    printf("Seu estado é o Maranhão e sua cidade é Água Doce, os números da sua placa é %d%d%d ", est, city, num);
+  } else if (est == 8 && city == 3) {
+    printf("Seu estado é o Maranhão e sua cidade é Aldeias Altas, os números da sua placa é %d%d%d ", est, city, num);
+  } else if (est == 8 && city == 4) {
+    printf("Seu estado é o Maranhão e sua cidade é Altamira do Maranhão, os números da sua placa é %d%d%d ", est, city, num);
+  } else if (est == 8 && city == 5) {
+    printf("Seu estado é o Maranhão e sua cidade é Alto Alegre do Maranhão, os números da sua placa é %d%d%d ", est, city, num);
+  } else if (est == 8 && city == 6) {
+    printf("Seu estado é o Maranhão e sua cidade é Alto Alegre do Pindaré, os números da sua placa é %d%d%d ", est, city, num);
+  } else if (est == 8 && city == 7) {
+    printf("Seu estado é o Maranhão e sua cidade é Alto Parnaíba, os números da sua placa é %d%d%d ", est, city, num);
+  } else if (est == 8 && city == 8) {
+    printf("Seu estado é o Maranhão e sua cidade é Amapá do Maranhão, os números da sua placa é %d%d%d ", est, city, num);
+  } else if (est == 8 && city == 9) {
+    printf("Seu estado é o Maranhão e sua cidade é Anajatuba, os números da sua placa é %d%d%d ", est, city, num);
+  }
+
+  // estado 9 e suas 10 primeiras cidades
+
+  if (est == 9 && city == 0) {
+    printf("Seu estado é o Mato Grosso e sua cidade é Acorizal, os números da sua placa é %d%d%d ", est, city, num);
+  } else if (est == 9 && city == 1) {
+    printf("Seu estado é o Mato Grosso e sua cidade é Água Boa, os números da sua placa é %d%d%d ", est, city, num);
+  } else if (est == 9 && city == 2) {
+    printf("Seu estado é o Mato Grosso e sua cidade é Alta Floresta, os números da sua placa é %d%d%d ", est, city, num);
+  } else if (est == 9 && city == 3) {
+    printf("Seu estado é o Mato Grosso e sua cidade é Alto Araguaia, os números da sua placa é %d%d%d ", est, city, num);
+  } else if (est == 9 && city == 4) {
+    printf("Seu estado é o Mato Grosso e sua cidade é Alto Boa Vista, os números da sua placa é %d%d%d ", est, city, num);
+  } else if (est == 9 && city == 5) {
+    printf("Seu estado é o Mato Grosso e sua cidade é Alto Graças, os números da sua placa é %d%d%d ", est, city, num);
+  } else if (est == 9 && city == 6) {
+    printf("Seu estado é o Mato Grosso e sua cidade é Alto Paraguai, os números da sua placa é %d%d%d ", est, city, num);
+  } else if (est == 9 && city == 7) {
+    printf("Seu estado é o Mato Grosso e sua cidade é Alto Taquari, os números da sua placa é %d%d%d ", est, city, num);
+  } else if (est == 9 && city == 8) {
+    printf("Seu estado é o Mato Grosso e sua cidade é Apiacás, os números da sua placa é %d%d%d ", est, city, num);
+  } else if (est == 9 && city == 9) {
+    printf("Seu estado é o Mato Grosso e sua cidade é Araguaiana, os números da sua placa é %d%d%d ", est, city, num);
+  }
+}
